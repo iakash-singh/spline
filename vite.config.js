8765ce68fa8a5568f786eb342d8e3076101b5bbf
akashsingh7202@gmail.com <akashsingh7202@gmail.com>
2025-02-26 22:34:18 +0530

    Ok

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/spline-design": {
+        target: "https://my.spline.design",
+        changeOrigin: true,
+        secure: false,
+        logLevel: "debug"
+      }
+    }
+  }
 })
+
+
+
